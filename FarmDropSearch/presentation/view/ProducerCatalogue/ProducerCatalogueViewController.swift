d5b3b54224e2e149766ba6c63500a70033266586
alasdair <alasdair.mccall@musicqubed.com>
2017-03-27 22:57:59 +0100

    added search bar to producer catalogue

--- FarmDropSearch/presentation/view/ProducerCatalogue/ProducerCatalogueViewController.swift
+++ FarmDropSearch/presentation/view/ProducerCatalogue/ProducerCatalogueViewController.swift
@@ -8,18 +8,41 @@
 
 import UIKit
 
-class ProducerCatalogueViewController: UIViewController, ProducerCatalogueUI, UITableViewDataSource, UITableViewDelegate {
+class ProducerCatalogueViewController: UIViewController, ProducerCatalogueUI, UITableViewDataSource, UITableViewDelegate, UISearchResultsUpdating, UISearchBarDelegate {
 
     private var producerCatalogue = ProducerCatalogue()
     
     @IBOutlet weak var tableView: UITableView!
     
+    var searchController: UISearchController!
+    
+    
     var presenter: ProducerCataloguePresenter! = nil
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
+        configureSearchController()
         presenter.viewDidLoad()
+    }
+    
+    
+    func configureSearchController() {
+        searchController = UISearchController(searchResultsController: nil)
+        searchController.searchResultsUpdater = self
+        searchController.dimsBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search for a producer..."
+        searchController.searchBar.delegate = self
+        searchController.searchBar.sizeToFit()
+        self.tableView.tableHeaderView = searchController.searchBar
+    }
+    
+    func updateSearchResults(for searchController: UISearchController) {
+//        <#code#>
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        //
     }
     
     func showLoading() {
